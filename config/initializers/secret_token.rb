fa97cd2d422efedf87af993791c99cf32d74c63e
Jair Zheng <jair.zheng@meginfo.com>
2012-07-10 09:23:44 +0800

    inital project
    use "rails new railsdemo"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsdemo::Application.config.secret_token = '05c5440f5466c035537e7b324a61f318a3e2350856d80b59c610e42116cebe0dce9e795a3b185703eb9f76c95c9a7a3c83c6c0f5b0bf76c7f69b7df43322a37f'

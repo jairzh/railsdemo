bfdda5abb461323f148b164cb9b8e0b506fa1989
Jair Zheng <jair.zheng@meginfo.com>
2012-07-30 08:46:08 +0800

    replace multiple items for a single product in a cart with a single item and reversible method
    $ rails generate migration combine_items_in_cart
    $ rake db:migrate
    $ rake db:rollback

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120726003754) do
+ActiveRecord::Schema.define(:version => 20120730000835) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false

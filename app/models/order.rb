4a954e23c98bab4b9a8b24b58c4bfd422f978d88
Jair Zheng <jair.zheng@meginfo.com>
2012-09-05 08:27:38 +0800

    define the data relationship

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,9 @@
 class Order < ActiveRecord::Base
   attr_accessible :address, :name, :email, :pay_type
   PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+
+  has_many :line_items, :dependent => :destroy
+
   validates :name, :address, :email, :pay_type, :presence => true
   validates :pay_type, :inclusion => PAYMENT_TYPES
 end

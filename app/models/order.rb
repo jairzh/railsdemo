cb63a23d84905cf6a596a6cd514c8027638f5c94
Jair Zheng <jair.zheng@meginfo.com>
2012-09-05 08:24:36 +0800

    add checkout button and update filed binding

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ActiveRecord::Base
-  attr_accessible :address, :name, :order_id
+  attr_accessible :address, :name, :email, :pay_type
+  PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
 end

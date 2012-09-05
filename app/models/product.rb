c4bc769f33b9a310f2d0babc1f76d2728ec64be6
Jair Zheng <jair.zheng@meginfo.com>
2012-09-05 08:20:18 +0800

    add Atom Feeds
    to test: curl --silent http://localhost:3000/products/3/who_bought.atom

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ActiveRecord::Base
 
   #data relationship
   has_many :line_items
+  has_many :orders, :through => :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
 
   private
@@ -24,4 +25,5 @@ class Product < ActiveRecord::Base
     :with    => %r{\.(gif|jpg|png)$}i,
     :message => 'must be a URL for GIF, JPG or PNG image.'
   }
+
 end
